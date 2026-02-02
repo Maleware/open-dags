91f08b88307731157f9e56462f2e6c111c4e8fbe
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-02 16:15:33 +0100

    new json approach

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -19,10 +19,12 @@ importlib.invalidate_caches()
 @dag(dag_id="clean-up-jobs", schedule="@daily", tags="clean_up")
 def clean_up_completed_jobs():
     #@task.bash(output_processor=lambda output: json.loads(output))
+    @task.bash
     def get_completed_jobs() -> str:
         return "/stackable/kubectl get pods -n stackable-products --output=json | jq '.items[] | select(.metadata.labels.\"app.kubernetes.io/component\" == \"spark\")'"
 #        return "/stackable/kubectl get pods -n stackable-products --output=json"
 #    @task.bash
+    @task
     def delete_completed_tasks(list: str):
  #       return f"/stackable/kubectl delete pod -n stackable-products {list}"
          print(f"{list}")
