16dffc8b3cca2e2153a77aa9820932bfd8f28e2c
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-02 14:15:45 +0100

    correct output processor declaration

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -15,11 +15,11 @@ print(sys.path)
 importlib.reload(site)
 importlib.invalidate_caches()
 
-@dag(dag_id="clean-up-jobs", schedule="@daily", tags="clean_up", output_processor=lambda output: json.loads(output))
+@dag(dag_id="clean-up-jobs", schedule="@daily", tags="clean_up")
 def clean_up_completed_jobs():
-    @task.bash
+    @task.bashi(output_processor=lambda output: json.loads(output))
     def get_completed_jobs() -> str:
-        return "/stackable/kubectl get pods -n stackable-products --output=json | jq '.items[] | select(.metadata.labels.\"app.kubernetes.io/component\" == \"spark\")'"
+        return "/stackable/kubectl get pods -n stackable-products --output=json | jq '.items[] | select(.metadata.labels.\"app.kubernetes.io/component\" == \"spark\")'
 
     @task.bash
     def delete_completed_tasks(list: str):
