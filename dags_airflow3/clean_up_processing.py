502d9486c275e92d81a9bac422f00140cbd1fdda
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-02 14:12:26 +0100

    escape "

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -19,7 +19,7 @@ importlib.invalidate_caches()
 def clean_up_completed_jobs():
     @task.bash
     def get_completed_jobs() -> str:
-        return "/stackable/kubectl get pods -n stackable-products --output=json | jq '.items[] | select(.metadata.labels."app.kubernetes.io/component" == "spark")'"
+        return "/stackable/kubectl get pods -n stackable-products --output=json | jq '.items[] | select(.metadata.labels.\"app.kubernetes.io/component\" == \"spark\")'"
 
     @task.bash
     def delete_completed_tasks(list: str):
