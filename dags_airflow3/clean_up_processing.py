86bdaa52576398a97b2675c69dd7e5eadb926761
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-02 18:07:23 +0100

    proper json usage

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -18,8 +18,8 @@ importlib.invalidate_caches()
 
 @dag(dag_id="clean-up-jobs", schedule="@daily", tags="clean_up")
 def clean_up_completed_jobs():
-    #@task.bash(output_processor=lambda output: json.loads(output))
-    @task.bash
+    @task.bash(output_processor=lambda output: json.loads(output))
+    #@task.bash
     def get_completed_jobs() -> str:
         #return "/stackable/kubectl get pods -n stackable-products --output=json | jq '.items[] | select(.metadata.labels.\"app.kubernetes.io/component\" == \"spark\")'"
         return "/stackable/kubectl get pods -n stackable-products --output=json"
@@ -31,6 +31,6 @@ def clean_up_completed_jobs():
         
     completed_tasks = get_completed_jobs()
     
-    delete_completed_tasks(completed_tasks)
+    delete_completed_tasks(json.dump(completed_tasks))
 
 clean_up_completed_jobs()
