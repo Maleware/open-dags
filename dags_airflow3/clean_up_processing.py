2450ab2aa24935f54eb9028da18fb4a38677acff
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-02 12:30:04 +0100

    Fixing syntax

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -18,11 +18,11 @@ importlib.invalidate_caches()
 @dag(dag_id="clean-up-jobs", schedule="@daily", tags="clean_up")
 def clean_up_completed_jobs():
     @task.bash
-    get_completed_jobs() -> str:
+    def get_completed_jobs() -> str:
         return "/stackable/kubectl get pods -n stackable-products | grep Completed"
 
     @task
-    delete_completed_tasks(list: str):
+    def delete_completed_tasks(list: str):
         print(f'{list}')
         
     completed_tasks = get_completed_jobs()
