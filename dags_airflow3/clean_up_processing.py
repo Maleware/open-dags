9ea526285b6c77c7768a10bc2291a736566eeb36
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-03 19:35:49 +0100

    add logging

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -31,8 +31,8 @@ def clean_up_completed_jobs():
         """
         cmd = f"/stackable/kubectl get pods -n {TARGET_NAMESPACE} | grep Completed"
         output = subprocess.check_output(cmd, shell=True)
-        json = json.load(output)
-        print(f"json: {json}")
+        json_string = json.load(output)
+        print(f"json: {json_string}")
         logging.debug(f"Cought pods from {TARGET_NAMESPACE}: {output}")
         # Output of subrocess contains leading b' from underlying data type.
         # Need to strip and split in lines per detected job
