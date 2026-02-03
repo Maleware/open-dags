2e94497ba1e3ce950dd9bbdf32eae3a1e6f1e4be
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-03 10:33:42 +0100

    Add filter for completed status

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -18,7 +18,7 @@ importlib.invalidate_caches()
 
 @dag(dag_id="clean-up-jobs", schedule="@daily", tags="clean_up")
 def clean_up_completed_jobs():
-    @task.bash(output_processor=lambda output: json.loads(output))
+    @task.bash(output_processor=lambda output: json.load(output))
     def get_completed_jobs() -> str:
         # return "/stackable/kubectl get pods -n stackable-products --output=json | jq -c '.items[] | select(.metadata.labels.\"app.kubernetes.io/component\" == \"spark\")'"
         return "/stackable/kubectl get pods -n stackable-products --output=json"
