3df1a54f62a3e893bb1abe574814375c44e7de84
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-03 13:33:18 +0100

    consolidate into one task

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -19,31 +19,32 @@ importlib.invalidate_caches()
 
 @dag(dag_id="clean-up-jobs", schedule="@daily", tags="clean_up")
 def clean_up_completed_jobs():
-    @task
     def get_completed_jobs() -> [str]:
         cmd = "/stackable/kubectl get pods -n stackable-products | grep Completed"
         output = subprocess.check_output(cmd, shell=True)
         print(output)
         return str(output).strip('b\'').split('\\n')
 
-    @task
     def filter_spark_job_names(pod_list: [str]) -> [str]:
         pods = []
         for pod_details in pod_list:
             pod_name = pod_details.split(' ')[0]
             if pod_name != '':
+                print(f"Found Pod: {pod_name}")
                 pods.append(pod_name)
-        return  pods
+        return pods
 
-    @task
     def delete_completed_spark_jobs(pod_names: [str]):
         for name in pod_names:
             cmd = f"/stackable/kubectl delete pod {name} -n stackable-products"
-            output = subprocess.run(cmd)
+            output = subprocess.run(cmd, shell=True)
             print(f"Deleted Pod: {output}")
 
-    completed_jobs = filter_spark_job_names(get_completed_jobs())
-    
-    delete_completed_spark_jobs(completed_jobs)
+    @task
+    def delete_completed_spark_jobs():
+        pod_names = filter_spark_job_names(get_completed_jobs())
+        delete_completed_spark_jobs(pod_names)
+
+    delete_completed_spark_jobs()
 
 clean_up_completed_jobs()
