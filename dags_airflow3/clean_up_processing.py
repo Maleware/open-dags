063e628f418c4b8183d1754058549ebd1ce7c349
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-03 11:43:48 +0100

    using python rather then bash

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -6,6 +6,7 @@ from airflow.sdk import dag, task
 from airflow.utils import yaml
 import os
 import sys
+import subprocess
 import importlib
 import site
 import json
@@ -18,11 +19,17 @@ importlib.invalidate_caches()
 
 @dag(dag_id="clean-up-jobs", schedule="@daily", tags="clean_up")
 def clean_up_completed_jobs():
-    @task.bash(output_processor=lambda output: json.dumps(output))
+    #@task.bash(output_processor=lambda output: json.loads(output))
     #@task.bash
+    @task
     def get_completed_jobs() -> str:
         # return "/stackable/kubectl get pods -n stackable-products --output=json | jq -c '.items[] | select(.metadata.labels.\"app.kubernetes.io/component\" == \"spark\")'"
-        return "/stackable/kubectl get pods -n stackable-products --output=json"
+        # return "/stackable/kubectl get pods -n stackable-products --output=json"
+        cmd = "/stackable/kubectl get pods -n stackable-products --output=json"
+        output= subprocess.check_output(cmd)
+        print(output)
+        output
+
     @task
     def filter_completed_spark_jobs(pod_list: str):
         pods = json.loads(pod_list)
