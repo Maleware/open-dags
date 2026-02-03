2c10ccbec99c072da695eea5a3cbf1d60bf14372
Maxi Wittich <maximilian.wittich@stackable.tech>
2026-02-03 14:10:57 +0100

    add logging

--- dags_airflow3/clean_up_processing.py
+++ dags_airflow3/clean_up_processing.py
@@ -1,8 +1,7 @@
-# k get pods -n stackable-products | grep Completed
-
 from airflow.sdk import dag, task
 import subprocess
 import importlib
+import logging
 import sys
 import site
 
@@ -14,26 +13,40 @@ importlib.invalidate_caches()
 
 TARGET_NAMESPACE="stackable-products"
 
-@dag(dag_id="clean-up-jobs", schedule="@daily", tags="clean_up")
+@dag(dag_id="clean-up-jobs", schedule="@daily", tags="cleanUp")
 def clean_up_completed_jobs():
     def get_completed_jobs() -> [str]:
+        """
+        Output of kubectl get pods | grep Completed looks something like
+        NAME                                                READY   STATUS      RESTARTS        AGE
+        airflow-postgresql-0                                1/1     Running     0               3h57m
+        airflow-scheduler-default-0                         3/3     Running     0               3h51m
+        airflow-triggerer-default-0                         3/3     Running     1 (3h51m ago)   3h51m
+        airflow-webserver-default-0                         3/3     Running     2 (3h50m ago)   3h51m
+        pyspark-pi-20260203125544-e0a3689c2392e824-driver   0/1     Completed   0               76s
+
+        grep for "Completed" only leaves lines with the podnames sperated by \\n"
+        """
         cmd = f"/stackable/kubectl get pods -n {TARGET_NAMESPACE} | grep Completed"
         output = subprocess.check_output(cmd, shell=True)
-        print(output)
+        logging.debug(f"Cought pods from {TARGET_NAMESPACE}: {output}")
+        # Output of subrocess contains leading b' from underlying data type.
+        # Need to strip and split in lines per detected job
         return str(output).strip('b\'').split('\\n')
 
     def filter_spark_job_names(pod_list: [str]) -> [str]:
         pods = []
         for pod_details in pod_list:
+            # Split by ' ' leaves a trailing empty entry
             pod_name = pod_details.split(' ')[0]
             if pod_name != '':
-                print(f"Found Pod: {pod_name}")
+                logging.debug(f"Found Pod: {pod_name}")
                 pods.append(pod_name)
         return pods
 
     def delete_jobs(pod_names: [str]):
         for name in pod_names:
-            print(f"ABOUT TO DELTE {name}")
+            logging.info(f"Deleting {name}")
             cmd = f"/stackable/kubectl delete pod {name} -n {TARGET_NAMESPACE}"
             output = subprocess.check_output(cmd, shell=True)
 
